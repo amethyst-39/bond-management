9b4e911151c6664b3581920c22e0f23716352f6e
Atharva Mahendra Belekar <70049793+ABelekar2101@users.noreply.github.com>
2023-08-09 13:42:50 +0530

    Implemented Security CRUD (#14)

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/DashBoard';
 import BondDetails from './components/BondDetails';
+import DeleteSecurity from './components/DeleteSecurity';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/bond" element={<BondDetails />} />
+        <Route path="/delete-security" element={<DeleteSecurity />} />
       </Routes>
     </Router>
   );
